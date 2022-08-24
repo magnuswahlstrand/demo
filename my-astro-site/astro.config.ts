a59af3a685b582b209b1dccde80f9086cca88982
Magnus <magnus@magnuss-mbp.lan>
2022-08-24 22:17:13 +0200

    add graph component

--- my-astro-site/astro.config.ts
+++ my-astro-site/astro.config.ts
@@ -17,52 +17,17 @@ export default defineConfig({
   integrations: [
     mdx({
       // apply remark-toc alongside GitHub-flavored markdown and Smartypants
-      remarkPlugins: {
-        extends: [
-          LOL({
-            classes: "px-5 py-2 bg-black w-min text-white rounded-t-md text-md",
-          }),
-        ],
-      },
+      // remarkPlugins: {
+      //   extends: [
+      //     LOL({
+      //       classes: "px-5 py-2 bg-black w-min text-white rounded-t-md text-md",
+      //     }),
+      //   ],
+      // },
     }),
     react(),
     tailwind(),
     // astroCodeSnippets(),
-    AutoImport({
-      imports: [
-        // Import a component’s default export
-        // generates:
-        // import A from './src/components/A.astro';
-        {
-          "./src/components/CodeSnippet/CodeSnippet.astro": [
-            ["default", "codesnippet"],
-          ],
-        },
-
-        {
-          // Explicitly alias a default export
-          // generates:
-          // import { default as B } from './src/components/B.astro';
-          // "./src/components/B.astro": [["default", "B"]],
-          // Import a module’s named exports
-          // generates:
-          // import { Tweet, YouTube } from 'astro-embed';
-          // "astro-embed": ["Tweet", "YouTube"],
-        },
-      ],
-    }),
-    // https://github.com/withastro/astro/issues/3637
-    // {
-    //   name: "inject-styles",
-    //   hooks: {
-    //     "astro:config:setup": ({ injectScript }) => {
-    //       injectScript(
-    //         "page-ssr",
-    //         `import ${CodeSnippetTagname} from "~/components/CodeSnippet/CodeSnippet.astro"; global.${CodeSnippetTagname} = ${CodeSnippetTagname};`
-    //       );
-    //     },
-    //   },
-    // },
   ],
   vite: {
     ssr: {
