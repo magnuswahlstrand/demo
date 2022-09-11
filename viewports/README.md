19c713b1dfcb6976be627e56cc2afe19dc3ee7f8
Magnus <magnus@magnuss-mbp.lan>
2022-09-11 11:05:01 +0200

    refactor

---
+++ viewports/README.md
@@ -0,0 +1,9 @@
+# A demo of viewports with React-Three-Fiber
+
+
+### TODO
+* [x] Fix center bug
+* [ ] Fix canvas scroll bug
+* [ ] Fix hover/touch controls bug
+* [ ] Write texts
+* [ ] Add wire button
--- viewports/src/App.tsx
+++ viewports/src/App.tsx
@@ -1,7 +1,7 @@
-import React, {useRef, useState} from "react";
+import React, {ReactElement, useRef, useState} from "react";
 
 import {Canvas, ThreeEvent} from "@react-three/fiber";
-import {Dodecahedron, OrbitControls, RoundedBox, View} from "@react-three/drei";
+import {Dodecahedron, OrbitControls, RoundedBox, Torus, View} from "@react-three/drei";
 
 // https://codesandbox.io/s/v5i9wl?file=/src/App.js:1194-1211
 
@@ -10,84 +10,78 @@ function Lights() {
         <>
             <ambientLight intensity={0.4}/>
             <pointLight position={[20, 30, 10]}/>
-            <pointLight position={[-10, -10, -10]} color="blue"/>
+            <pointLight position={[-10, -10, -10]} color="hotpink"/>
         </>
     )
 }
 
 interface MyViewProps {
     trackingRef: React.MutableRefObject<HTMLElement>
+    wireframe: boolean
+    children: ReactElement
 }
 
-function useHoveredMesh() {
+function ShapeView({trackingRef, wireframe, children}: MyViewProps) {
     const [hovered, setHover] = useState(false)
-    const shapeProps = {
-        onPointerOver: (e: ThreeEvent<MouseEvent>) => setHover(true),
-        onPointerOut: (e: ThreeEvent<MouseEvent>) => setHover(false),
-    }
-    const materialProps = {
-        color: hovered ? "hotpink" : "#f5efe6",
-        wireframe: false,
-    }
-    return {shapeProps, materialProps};
-}
 
-function MyView({trackingRef}: MyViewProps) {
-    const {shapeProps, materialProps} = useHoveredMesh();
+    const shape = React.cloneElement(
+        children,
+        {
+            onPointerOver: (e: ThreeEvent<MouseEvent>) => setHover(true),
+            onPointerOut: (e: ThreeEvent<MouseEvent>) => setHover(false),
+        },
+        <meshPhongMaterial
+            color={hovered ? "hotpink" : "#f5efe6"}
+            wireframe={wireframe}
+        />
+    )
 
     return (<View track={trackingRef}>
+        <color attach="background" args={['black']}/>
         <Lights/>
-        {/*<color attach="background" args={['black']}/>*/}
         <mesh>
-            {/*{childStart}*/}
-            <Dodecahedron args={[2, 0]} {...shapeProps}>
-                <meshPhongMaterial {...materialProps}/>
-            </Dodecahedron>
-            {/*{childStop}*/}
+            {shape}
         </mesh>
-        <mesh/>
     </View>)
 }
 
 function App() {
     const container = useRef<HTMLElement>(null)
-    const tracking = useRef<HTMLDivElement>(null)
-    const tracking2 = useRef<HTMLDivElement>(null)
+    const view1 = useRef<HTMLDivElement>(null)
+    const view2 = useRef<HTMLDivElement>(null)
+    const view3 = useRef<HTMLDivElement>(null)
+    const [wireframe, setWireframe] = useState(false);
+
+    console.log(wireframe)
 
     return (
-        <main ref={container} className={"container mx-auto"}>
-            <Canvas eventSource={container.current} className={"canvas"}>
+        <main ref={container} className={"container mx-auto h-full"}>
+            <div className="flex flex-row justify-center text-white">
 
+                <div className="grid grid-cols-1 place-items-end sm:grid-cols-2 bg-stone-700  z-0 inline-grid">
+                    <div className="h-48 w-48 view1  z-0 place-self-start" ref={view1}/>
+                    <div className="h-48 w-48 z-50 place-self-start">Some text here</div>
+                    <div className="h-48 w-48 z-50">More text here
+                        <button className={"bg-blue-900"} onClick={() => setWireframe((prev) => !prev)}>Show
+                            wireframe</button>
+                    </div>
+                    <div className="h-48 w-48 view2 z-0" ref={view2}/>
+                    <div className="h-48 w-48 view1 z-0" ref={view3}/>
+                    <div className="h-48 w-48 z-50">Some text here</div>
+                </div>
+                <Canvas eventSource={container} className={"canvas z-40"}>
+                    <OrbitControls/>
+                    <ShapeView trackingRef={view1} wireframe={wireframe}>
+                        <Dodecahedron args={[2, 0]}/>
+                    </ShapeView>
+                    <ShapeView trackingRef={view2} wireframe={wireframe}>
+                        <RoundedBox args={[3, 3, 3]} radius={0.2}/>
+                    </ShapeView>
+                    <ShapeView trackingRef={view3} wireframe={wireframe}>
+                        <Torus args={[1.8, 0.5, 16, 20]}/>
+                    </ShapeView>
+                </Canvas>
 
-                <OrbitControls/>
-                <MyView trackingRef={tracking}/>
-                <View track={tracking2}>
-                    <Lights/>
-                    <color attach="background" args={['black']}/>
-                    {/*<color attach="background" args={"black"} />*/}
-                    <mesh>
-                        <RoundedBox args={[2, 2, 2]} radius={0.2}
-                                    onClick={() => {
-                                        console.log("clicked")
-                                    }}
-                                    onPointerEnter={() => console.log('yeah')}
-                                    onPointerLeave={() => console.log('yeah')}
-                                    onPointerOver={() => console.log('yeah')}
-                                    onPointerOut={() => console.log('yeah')}
-                        >
-                            <meshPhongMaterial color="#ffffff"/>
-                        </RoundedBox>
-                    </mesh>
-                    <mesh/>
-                </View>
-            </Canvas>
-            <div className="grid grid-cols-2 bg-amber-600 w-96 mx-auto ">
-                <div className="border-2 border-black h-48 w-48 view1" ref={tracking}
-                     onClick={() => console.log('yea2')}/>
-                <div className="border-2 border-black h-48 w-48">Some text here</div>
-                <div className="border-2 border-black h-48 w-48">More text here</div>
-                <div className="border-2 border-black h-48 w-48 view2" ref={tracking2}
-                     onClick={() => console.log('yea3')}/>
             </div>
         </main>
     )
