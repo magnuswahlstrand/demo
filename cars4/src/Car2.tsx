7ddb118b4302cc6d6a2c22c2121f07413e890c0a
Magnus <magnus@magnuss-mbp.lan>
2022-09-17 13:13:52 +0200

    movable character, velocity based movement

--- cars4/src/Car2.tsx
+++ cars4/src/Car2.tsx
@@ -3,9 +3,15 @@ import {useRef, useState} from "react";
 import {CapsuleCollider, RigidBody, RigidBodyApi, useRapier} from "@react-three/rapier";
 import {useFrame} from "@react-three/fiber";
 import * as THREE from "three";
-import { useControls } from 'leva'
+import {useControls} from 'leva'
 
 export const Car2 = () => {
+    const {velocity, angularVelocity} = useControls(
+        {
+            velocity: {value: 10, min: 1, max: 20, step: 1},
+            angularVelocity: {value: 3, min: 1, max: 10, step: 1},
+        })
+
     const [colliding, setColliding] = useState(new Set());
     const {controls} = useControlContext()
     const ref = useRef<RigidBodyApi>(null);
@@ -25,18 +31,18 @@ export const Car2 = () => {
         // console.log(ref.current.rotation().y*Math.PI/2)
         // console.log(controls.controls.current.down)
         // if(controls.controls.current)
-        let av = 0
-        let v = 0
-        v += controls.current.up ? 10 : 0
-        v += controls.current.down ? -10 : 0
-        av += controls.current.left ? 3 : 0
-        av += controls.current.right ? -3 : 0
+        let dAV = 0
+        let dV = 0
+        dV += controls.current.up ? velocity : 0
+        dV += controls.current.down ? -velocity : 0
+        dAV += controls.current.left ? angularVelocity : 0
+        dAV += controls.current.right ? -angularVelocity : 0
 
         // TODO: Reuse vector v
-        ref.current.setAngvel(new THREE.Vector3(0, av, 0))
+        ref.current.setAngvel(new THREE.Vector3(0, dAV, 0))
         // TODO: refactor?
         if (colliding.size > 0) {
-            ref.current.setLinvel(new THREE.Vector3(v, ref.current.linvel().y, 0).applyQuaternion(ref.current.rotation()))
+            ref.current.setLinvel(new THREE.Vector3(dV, ref.current.linvel().y, 0).applyQuaternion(ref.current.rotation()))
         }
         // forward velocity
         // ref.current.setLinvel(new THREE.Vector3(3, 0, 0).applyAxisAngle(yAxis, ref.current.rotation().y))
@@ -56,14 +62,15 @@ export const Car2 = () => {
     })
 
 
-    console.log(rapier)
-    console.log(colliding)
+    // console.log(rapier)
+    // console.log(colliding)
+    if (ref.current) console.log(ref.current.collider)
     // console.log(ref.current)
 
     return <RigidBody
+        ref={ref}
         linearDamping={0.5}
         name="playar"
-        ref={ref}
         position={[-3, 2, 0]} colliders={false}
         onCollisionEnter={(payload) => {
             // TODO: Ask community how to check for ground collision. This is ugly hack, and most likely error prone
@@ -80,16 +87,14 @@ export const Car2 = () => {
             <capsuleGeometry args={[0.5, 1, 4, 8]}/>
             <meshPhysicalMaterial color="orange"/>
         </mesh>
-        <CapsuleCollider args={[0.5, 0.5]}
-
-
-
-                         onCollisionEnter={payload => {
-                             console.log(payload)
-                         }}
-                         onCollisionExit={payload => {
-                             console.log(payload)
-                         }}
+        <CapsuleCollider
+            args={[0.5, 0.5]}
+            onCollisionEnter={payload => {
+                console.log(payload)
+            }}
+            onCollisionExit={payload => {
+                console.log(payload)
+            }}
         />
     </RigidBody>
 }
