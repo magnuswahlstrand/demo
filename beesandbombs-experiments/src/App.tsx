47ec41e125b3763ffedc30992853928f097deccc
Magnus <magnus@magnuss-mbp.lan>
2022-09-22 09:23:53 +0200

    first complete version

--- beesandbombs-experiments/src/App.tsx
+++ beesandbombs-experiments/src/App.tsx
@@ -1,12 +1,9 @@
-import {Canvas, useFrame} from '@react-three/fiber';
-import {Box, Line, OrbitControls} from "@react-three/drei";
-import {animated, config, useChain, useSpring, useSpringRef, useTrail} from '@react-spring/three';
-import React from "react";
-import {Vector3} from "three";
+import {Canvas} from '@react-three/fiber';
+import {Line, OrbitControls} from "@react-three/drei";
+import {animated, useSpring, useTrail} from '@react-spring/three';
+import React, {useState} from "react";
 
-export const initialCamera = new Vector3(4, 0, 0)
-
-const points: [number, number, number][] = [
+const squarePoints: [number, number, number][] = [
     [0, -0.5, -0.5],
     [0, -0.5, 0.5],
     [0, 0.5, 0.5],
@@ -14,7 +11,7 @@ const points: [number, number, number][] = [
     [0, -0.5, -0.5],
 ]
 
-const points2: [number, number, number][] = [
+const twistedSquarePoints: [number, number, number][] = [
     [0, -0.5, 0.5],
     [0, 0.5, 0.5],
     [0, 0.5, -0.5],
@@ -22,123 +19,137 @@ const points2: [number, number, number][] = [
     [0, -1.5, -1.5],
     [0, -0.5, -1.5],
     [0, -0.5, 0.5],
-].map(([x, y, z]) => [x, y+0.5, z+0.5])
+].map(([x, y, z]) => [x, y + 0.5, z + 0.5])
 
 const AnimatedLine = animated(Line);
-const AnimatedBox = animated(Box);
-
-function SquareLine({posX, delay}: { posX: number, delay: number }) {
-    const s = useSpring({
-        to: {"rotation-x": -Math.PI / 2},
-        from: {"rotation-x": 0},
-        loop: {reverse: false, delay: 1000, reset: true},
-        delay: delay,
-        config: config.slow,
-    })
-
-    return <AnimatedLine
-        rotation={[0, 0, 0]}
-        {...s}
-        position={[posX, 0, 0]}
-        points={points}
-        color={"black"}
-        lineWidth={1.5}
-    />;
-}
-
 
 const offset = 0.202
+const n = 12
+const n2 = n / 2
 
-function Scene() {
-    const n = 12
-    const n2 = 6
+const baseSquares = [...Array(n)].map((v, i) => ({
+    posX: offset * (i - (n - 1) / 2)
+}))
 
-    const groupRef = useSpringRef();
-    const {rotY} = useSpring({
-        ref: groupRef,
-        "rotY": Math.PI,
-        from: {"rotY": 0},
-    })
+const twistedSquares = [...Array(n2)].map((v, i) => ({
+    posX: offset * (i - (n2 - 1) / 2),
+}))
 
-    const items = [...Array(n)].map((v, i) => ({
-        posX: offset * i - offset * n / 2,
-    }))
-    const items2 = [...Array(6)].map((v, i) => ({
-        posX: offset * i - offset * n2 / 2,
-    }))
+interface StepProps {
+    onComplete: () => void
+}
 
-    const trailRef = useSpringRef();
-    const trail = useTrail(items.length, {
-        ref: trailRef,
+// Rotate each individual square 90 degrees
+function RotatingSquares({onComplete}: StepProps) {
+    const offsets = baseSquares
+    const trail = useTrail(offsets.length, {
         to: {"rotation-x": -Math.PI / 2},
         from: {"rotation-x": 0},
-        config: config.stiff,
-        pause:  true,
+        config: {precision: 0.001, duration: 400},
+        // config: {...config.stiff, precision: 0.01},
+        onRest: (_, w) => {
+            if (w.springs["rotation-x"]._priority === offsets.length - 1) onComplete()
+        },
     })
-    const trail2Ref = useSpringRef();
-    const trail2 = useTrail(items2.length, {
-        ref: trail2Ref,
-        to: {"rotation-x": -Math.PI/2},
-        from: {"rotation-x": 0},
-        config: config.slow,
-        onRest: () => {
-            console.log("rest")
-            trail2Ref.start({"rotation-x": -Math.PI, delay: 1000})
-        }
-    })
-
-
-    useChain([])
 
     return <>
-        <animated.group
-            rotation-y={rotY}
-        >
-            {/*{trail.map((props, i) => (*/}
-            {/*    <AnimatedLine*/}
-            {/*        key={i}*/}
-            {/*        rotation={[0, 0, 0]}*/}
-            {/*        {...props}*/}
-            {/*        position={[items[i].posX, 0, 0]}*/}
-            {/*        points={points}*/}
-            {/*        lineWidth={1.5}*/}
-            {/*    />*/}
-            {/*))}*/}
-        </animated.group>
-        {trail2.map((props, i) => (
+        {trail.map((props, i) => (
+            // @ts-ignore - type instantiation is excessively deep
             <AnimatedLine
                 key={i}
                 rotation={[0, 0, 0]}
                 {...props}
-                position={[items2[i].posX, 0, 0]}
-                points={points2}
-                color={"blue"}
-                lineWidth={2}
+                position={[offsets[i].posX, 0, 0]}
+                points={squarePoints}
+                lineWidth={1.5}
             />
         ))}
-        {/*{items2.map((v, i) => (<AnimatedLine*/}
-        {/*    position={[items[i].posX, 0, 0]}*/}
-        {/*    points={points2}*/}
-        {/*    color={"red"}*/}
-        {/*    lineWidth={4}*/}
-        {/*/>))}*/}
     </>
+}
+
+// Rotate all squares 180 degrees as a group
+function RotatingGroup({onComplete}: StepProps) {
+    const offsets = baseSquares
+    const {rotY} = useSpring({
+        "rotY": -Math.PI,
+        from: {"rotY": 0},
+        config: {mass: 1, tension: 50, precision: 0.001},
+        onRest: () => onComplete(),
+    })
+    return (<animated.group
+        rotation-y={rotY}
+    >
+        {offsets.map((props, i) => (
+            <AnimatedLine
+                key={i}
+                rotation={[0, 0, 0]}
+                position={[offsets[i].posX, 0, 0]}
+                points={squarePoints}
+                lineWidth={1.5}
+            />
+        ))}
+    </animated.group>)
+}
+
+
+// Rotate each combined square 90 degrees
+function RotatingTwistedSquares({onComplete, rotFromX}: StepProps & { rotFromX: number }) {
+    const offsets = twistedSquares
+    const trail = useTrail(offsets.length, {
+        to: {"rotation-x": -rotFromX - Math.PI / 2},
+        from: {"rotation-x": -rotFromX},
+        config: {mass: 1, tension: 100, precision: 0.001, duration: 600},
+        onRest: (_, w) => {
+            if (w.springs["rotation-x"]._priority === offsets.length - 1) onComplete()
+        },
+    })
+
+    return <>
+        {trail.map((props, i) => (
+            <AnimatedLine
+                key={i}
+                rotation={[0, 0, 0]}
+                {...props}
+                position={[offsets[i].posX, 0, 0]}
+                points={twistedSquarePoints}
+                color={"black"}
+                lineWidth={1.5}
+            />
+        ))}
+    </>
+}
+
+function Scene() {
+    const [step, setStep] = useState(0);
+
+    const nSteps = 3
+
+    const stepForward = () => {
+        setStep((step) => (step + 1) % nSteps)
+    }
+
+    switch (step) {
+        case 0:
+            return <RotatingSquares onComplete={stepForward}/>
+        case 1:
+            return <RotatingTwistedSquares onComplete={stepForward} rotFromX={0}/>
+        case 2:
+            return <RotatingTwistedSquares onComplete={stepForward} rotFromX={Math.PI / 2}/>
+        default:
+            throw new Error("Unknown step")
+
+    }
 }
 
 export default function App() {
     return (
         <Canvas dpr={window.devicePixelRatio} orthographic
-                // Calculated by fair dice roll.
+            // Position found using orbitControls :-)
                 camera={{zoom: 100, position: [10, 8.27, 8.27]}}>
             <color attach="background" args={['white']}/>
             <pointLight position={[10, 10, 10]}/>
             <pointLight position={[-10, 10, 5]}/>
             <OrbitControls/>
-            {/*<gridHelper*/}
-            {/*    args={[100, 100, "#a0ffa0", "#a0a0ff"]}*/}
-            {/*    position={[0, 0, 0]}*/}
-            {/*/>*/}
-            {/*<axesHelper args={[4]}/>*/}
             <Scene/>
         </Canvas>
     );
