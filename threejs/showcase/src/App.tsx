2cbc5f21f46a41618e26d5fc385fdf9a81e024b7
Magnus <magnus@magnuss-mbp.lan>
2022-10-06 10:11:16 +0200

    Add rotating squares

--- threejs/showcase/src/App.tsx
+++ threejs/showcase/src/App.tsx
@@ -3,29 +3,32 @@ import { Application, Description, Example, Heading } from "r3f-stage"
 
 import Clump from "./examples/Clump"
 import ColoredLines from "./examples/ColoredLine"
+import RotatingSquares from "./examples/RotatingSquares"
 /* r3f-stage provides a global stylesheet. Please import it in your application and remove any other global styles you may have defined. */
 import "r3f-stage/styles.css"
 
 function App() {
   return (
+
     <Application>
-      <Example path="one" title="Example 1" makeDefault>
-        <Description>This is a very simple example.</Description>
 
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial color="green" />
-        </mesh>
-      </Example>
+      <Heading>Physics</Heading>
 
-      <Example path="clump" title="Clumping">
+      <Example path="clump" title="Clumping" makeDefault>
         <Description>Move the cursor or click the screen to break the formation.</Description>
         <Clump />
       </Example>
+
+      <Heading>Graphics</Heading>
       <Example path="colored-lines" title="Colored Lines">
-        <Description>Move the cursor or click the screen to break the formation.</Description>
         <ColoredLines />
       </Example>
+
+      <Example path="rotating-squares" title="Rotating Squares">
+        <Description>Move the cursor or click the screen to break the formation.</Description>
+        <RotatingSquares />
+      </Example>
+
     </Application>
   )
 }
---
+++ threejs/showcase/src/examples/RotatingSquares.tsx
@@ -0,0 +1,149 @@
+import {Canvas} from '@react-three/fiber';
+import { Line, OrbitControls, OrthographicCamera } from "@react-three/drei";
+import {animated, useSpring, useTrail} from '@react-spring/three';
+import React, {useState} from "react";
+
+const AnimatedLine = animated(Line);
+
+const square: [number, number, number][] = [
+  [0, -0.5, -0.5],
+  [0, -0.5, 0.5],
+  [0, 0.5, 0.5],
+  [0, 0.5, -0.5],
+  [0, -0.5, -0.5],
+]
+
+const twistedSquares: [number, number, number][] = [
+  [0, 0, 1],
+  [0, 1, 1],
+  [0, 1, 0],
+  [0, -1, 0],
+  [0, -1, -1],
+  [0, 0, -1],
+  [0, 0, 1],
+]
+
+const OFFSET = 0.202
+const COUNT = 12
+
+const offsetArray = (offset: number, n: number) => [...Array(n)].map((v, i) => ({
+  posX: offset * (i - (n - 1) / 2)
+}))
+
+
+interface StepProps {
+  onComplete: () => void
+}
+
+// Rotate each individual square 90 degrees
+function RotatingSquares({onComplete}: StepProps) {
+  const offsets = offsetArray(OFFSET, COUNT)
+  const trail = useTrail(offsets.length, {
+    to: {"rotation-x": -Math.PI / 2},
+    from: {"rotation-x": 0},
+    config: {precision: 0.001},
+    onRest: (_, w) => {
+      if (w.springs["rotation-x"]._priority === offsets.length - 1) onComplete()
+    },
+  })
+
+  return <>
+    {trail.map((props, i) => (
+      // @ts-ignore - type instantiation is excessively deep
+      <AnimatedLine
+        key={i}
+        rotation={[0, 0, 0]}
+        {...props}
+        position={[offsets[i].posX, 0, 0]}
+        points={square}
+        lineWidth={1.5}
+      />
+    ))}
+  </>
+}
+
+// Rotate all squares 180 degrees as a group
+function RotatingGroup({onComplete}: StepProps) {
+  const offsets = offsetArray(OFFSET, COUNT)
+  const {rotY} = useSpring({
+    "rotY": -Math.PI,
+    from: {"rotY": 0},
+    config: {mass: 1, tension: 50, precision: 0.001},
+    onRest: () => onComplete(),
+  })
+  return (<animated.group
+    rotation-y={rotY}
+  >
+    {offsets.map((props, i) => (
+      <AnimatedLine
+        key={i}
+        rotation={[0, 0, 0]}
+        position={[offsets[i].posX, 0, 0]}
+        points={square}
+        lineWidth={1.5}
+      />
+    ))}
+  </animated.group>)
+}
+
+
+// Rotate each combined square 90 degrees
+function RotatingTwistedSquares({onComplete, rotFromX}: StepProps & { rotFromX: number }) {
+  const offsets = offsetArray(OFFSET, COUNT / 2)
+  const trail = useTrail(offsets.length, {
+    to: {"rotation-x": -rotFromX - Math.PI / 2},
+    from: {"rotation-x": -rotFromX},
+    config: {mass: 1, tension: 100, precision: 0.001},
+    onRest: (_, w) => {
+      if (w.springs["rotation-x"]._priority === offsets.length - 1) onComplete()
+    },
+  })
+
+  return <>
+    {trail.map((props, i) => (
+      <AnimatedLine
+        key={i}
+        rotation={[0, 0, 0]}
+        {...props}
+        position={[offsets[i].posX, 0, 0]}
+        points={twistedSquares}
+        color={"black"}
+        lineWidth={1.5}
+      />
+    ))}
+  </>
+}
+
+function Scene() {
+  const [step, setStep] = useState(0);
+
+  const nSteps = 3
+
+  const stepForward = () => {
+    setStep((step) => (step + 1) % nSteps)
+  }
+
+  switch (step) {
+    case 0:
+      return <RotatingSquares onComplete={stepForward}/>
+    case 1:
+      return <RotatingTwistedSquares onComplete={stepForward} rotFromX={0}/>
+    case 2:
+      return <RotatingTwistedSquares onComplete={stepForward} rotFromX={Math.PI / 2}/>
+    default:
+      throw new Error("Unknown step")
+
+  }
+}
+
+export default function App() {
+  return (<>
+            <OrthographicCamera makeDefault
+  position={[10, 8.27, 8.27]} zoom={100}/>
+      <color attach="background" args={['white']}/>
+      {/*<pointLight position={[10, 10, 10]}/>*/}
+      {/*<pointLight position={[-10, 10, 5]}/>*/}
+      <Scene/>
+    </>
+  );
+}
