62dad0c00766064a1d926515616cf7e924b5b794
Magnus <magnus@magnuss-mbp.lan>
2022-10-19 15:55:26 +0200

    improved version

--- threejs/trine-copy/src/App.tsx
+++ threejs/trine-copy/src/App.tsx
@@ -1,8 +1,10 @@
 import type { Vector3 } from "@react-three/drei";
 import { Canvas, mesh, useFrame } from "@react-three/fiber";
 import { Debug, Physics, RigidBody, RigidBodyApi } from "@react-three/rapier";
-import { useRef, useState } from "react";
-import { PerspectiveCamera, Plane, Vector2, Vector3 as V3 } from "three";
+import { useEffect, useRef, useState } from "react";
+import { Plane, Vector3 as V3 } from "three";
+import { useDrag } from "@use-gesture/react";
+import { damp3 } from "maath/easing";
 
 const width = 1;
 
@@ -24,53 +26,38 @@ function MyPlane2({ position }: { position?: Vector3, rotation?: Vector3 }) {
   </RigidBody>;
 }
 
-function updatePlanXZPosition(vec: Vector3, mouse: Vector2, camera: PerspectiveCamera) {
-  vec.set(
-    mouse.x,
-    mouse.y,
-    0.0);
-
-  vec.unproject(camera);
-  vec.sub(camera.position).normalize();
-  const distance = -camera.position.z / vec.z;
-  vec.multiplyScalar(distance).add(camera.position);
-  vec.setZ(0);
-}
-
-
 const floorPlane = new Plane(new V3(0, 0, 1), 0);
 
-const e2 = new V3();
+const mouse = new V3();
+const target = new V3();
 
-function MyPlane3({ position }: { position?: Vector3, rotation?: Vector3 }) {
+interface SceneProps {
+  position: Vector3,
+  target?: Vector3
+  mouse: Vector3
+}
+
+function MyPlane3({
+                    position,
+                    mouse,
+                    target = new V3(),
+                  }: SceneProps) {
   const [selected, setSelected] = useState(false);
   const [pos, setPos] = useState([...position]);
   const ref = useRef<RigidBodyApi>(null!);
-  // const e = new V3(0, 0, 1);
-  // const e2 = new V3(pos[0], pos[1], pos[2]);
 
-  console.log("po", pos);
-
-  useFrame((state) => {
-    e2.set(pos[0], pos[1], pos[2]);
-    console.log(e2);
-    // if (selected) {
-      ref.current.setNextKinematicTranslation(e2);
-    // }
+  const bind = useDrag(({ down, movement: [mx, my], event }) => {
+    event.ray.intersectPlane(floorPlane, mouse);
   });
 
-  const pointerMove = (e: MouseEvent) => {
-    if (selected) {
-      e.ray.intersectPlane(floorPlane, e2);
-      console.log(e2);
-      setPos([e2.x, e2.y, e2.z]);
-    }
-  };
+  useFrame((state, dt) => {
+    damp3(target, mouse, 0.05, dt, 10);
+    ref.current.setNextKinematicTranslation(target);
+  });
 
   return <RigidBody mass={1} ref={ref} type={"kinematicPosition"} position={position}>
     <mesh rotation={[-Math.PI / 2, 0, 0]}
-          onClick={() => setSelected(!selected)}
-          onPointerMove={pointerMove}
+          {...bind()}
     >
       <boxGeometry args={[width, width, 0.1]} />
       <meshBasicMaterial color="blue" />
@@ -86,7 +73,10 @@ function Scene() {
       <MyPlane />
       <MyPlane position={[1.5 * width, 0.2, 0]} />
       <MyPlane2 position={[0.75 * width, 0.5, 0]} />
-      <MyPlane3 position={[0.75 * width, 2.5, 0]} />
+      <MyPlane3 position={[0.75 * width, 2.5, 0]}
+                mouse={new V3(0.75 * width, 2.5, 0)}
+                target={new V3(0.75 * width, 2.5, 0)}
+      />
     </Physics>
 
   </>;
